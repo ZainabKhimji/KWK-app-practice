f629ab2cd36c0b1990b3a08bf7cedd543b346c32
ZainabKhimji <zakhimji@gmail.com>
2023-06-23 12:28:56 -0500

    Initial Commit

---
+++ Day 4 practice/Day_4_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day_4_practiceApp.swift
+//  Day 4 practice
+//
+//  Created by Zainab Khimji on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Day_4_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
